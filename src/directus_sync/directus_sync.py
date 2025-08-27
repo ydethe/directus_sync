ef16c10957035c2a9dbe3344339882003e233e0c
Yann de The <ydethe@gmail.com>
2025-08-27 16:26:53 +0200

    Initial commit

--- src/directus_sync/directus_sync.py
+++ src/directus_sync/directus_sync.py
@@ -4,6 +4,7 @@ from .config import Config
 
 
 def export(config: Config):
+    # https://directus.io/docs/getting-started/use-the-api#fetching-data
     res = requests.get(
         str(config.directus_url), headers={"Authorization": f"Bearer {config.directus_token}"}
     )
