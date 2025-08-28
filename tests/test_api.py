bdb9d0db693adb42767727247121683c58bf2b13
Yann de The <ydethe@gmail.com>
2025-08-28 11:42:04 +0200

    add vcard pydantic model generated by chatgpt

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,11 +4,37 @@ import json
 import yaml
 from yaml import BaseLoader
 
+from directus_sync.directus_backend import read_contacts
 from directus_sync.directus_sync import export
 from directus_sync.models import Config
+from directus_sync.vcard import Name, VCard
 
 
 class TestDirectusSync(unittest.TestCase):
+    def test_vcard(self):
+        with open("tests/prod.yml", "r") as f:
+            dat = yaml.load(f, Loader=BaseLoader)
+        config = Config.model_validate_json(json.dumps(dat))
+        for contact in read_contacts(config):
+            vcard = VCard(
+                fn=f"{contact.Prenom}{contact.Particule}{contact.Nom}",
+                n=Name(
+                    family=f"{contact.Particule}{contact.Nom}".strip(),
+                    given=contact.Prenom,
+                    prefixes=[contact.Civilite],
+                ),
+                anniversary=contact.Date_de_naissance,
+                # gender: Optional[Gender] = None
+                # adr: Optional[List[Address]] = None
+                # tel: Optional[List[Telephone]] = None
+                # email: Optional[List[Email]] = None
+                title=contact.Civilite,
+                # role: Optional[str] = None
+                # org: Optional[Organization] = None
+            )
+            dscard = vcard.to_vcard()
+            print(dscard)
+
     def test_sync_all(self):
         with open("tests/prod.yml", "r") as f:
             dat = yaml.load(f, Loader=BaseLoader)
@@ -19,4 +45,5 @@ class TestDirectusSync(unittest.TestCase):
 if __name__ == "__main__":
     a = TestDirectusSync()
 
-    a.test_sync_all()
+    a.test_vcard()
+    # a.test_sync_all()
